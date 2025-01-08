48fbbdde721746da76394a233aee47dad1913317
Joaogdf <joaogustavone@gmail.com>
2025-01-08 23:14:40 +0000

    feat: add data to project

---
+++ app/_data/watchlist.ts
@@ -0,0 +1,17 @@
+export const watchlistData = [
+  {
+    viewAllText: "My TV",
+    sectionName: "Watchlist",
+    categoryImage: "/cinema/category/cinema.png",
+    bannerImage: "/cinema/banners/pulpfiction.jpg",
+    title: "Pulp Fiction",
+    description:
+      "Quentin Tarantino's iconic crime drama follows the intertwining tales of mobsters and criminals on the streets of LA. With John Travolta, Bruce Willis and Samuel L Jackson.",
+    classification: "S",
+    classAge: "18",
+    duration: "2h 28m",
+    year: "1994",
+    genre: "Thriller",
+    altText: "Pulp Fiction",
+  },
+];
